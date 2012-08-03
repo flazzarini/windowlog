ec6c7f2def169dfd4dd600d40d461315c2ff9f19
Frank Lazzarini <flazzarini@gmail.com>
2012-08-03 16:06:05 +0200

    - Added screenshot feature - Less messages on console show only info when window was changed - Initiate class just once

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import logging, subprocess, sys
 from time import sleep
 from sqlalchemy import create_engine, MetaData, Column, Table, ForeignKey, Integer, String
+from uuid import uuid1
 
 
 # Logging configuration
@@ -9,26 +10,22 @@ logging.basicConfig()
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-# Database configuration
-engine = create_engine('sqlite:///windowlog.db', echo=True)
-metadata = MetaData(bind=engine)
-	
-windowlog = Table('windowlog', metadata,
-		  Column('moment', Integer, primary_key=True),
-		  Column('windowname', String(40)))
-
-metadata.create_all()
-connection = engine.connect()
-
 
 class window :
 	def __init__(self) :
+		logger.debug('---> Window Class initialized <---')
 		self.id    = self.getWindowId()
 		self.oldid = self.id
 		self.name  = self.getWindowName(self.id)
 		self.pid   = self.getWindowPid(self.id)
 		self.bin   = self.getWindowBin(self.id)
-		logger.info('---> Window Class initialized <---')
+		self.getInfo()
+
+	def getInfo(self) :
+		logger.info('Window id   : %s'   % self.id)
+		logger.info('Window name : %s'   % self.name)
+		logger.info('Window pid  : %s'   % self.pid)
+		logger.info('Window bin  : %s'   % self.bin)
 
 	def getWindowId(self) :
 		cmd = "xprop -root | grep '_NET_ACTIVE_WINDOW(WINDOW)' | cut -d ' ' -f 5 | tr -d '\n'"
@@ -37,19 +34,19 @@ class window :
 		logger.debug('winIdError  : %s' % winIdError)
 
 		if winIdError == '' :
-			logger.info('Current Window Id : %s' % winIdOutput)
+			logger.debug('Current Window Id : %s' % winIdOutput)
 			return winIdOutput
 		else :
 			return None
 
 	def getWindowName(self, id) :
-		cmd = "xwininfo -id %s | grep 'xwininfo: Window id:' | awk 'BEGIN {FS=\"\\\"\"}/xwininfo: Window     id/{print $2}' | tr -d '\n'" % id
+		cmd = "xwininfo -id %s | grep 'xwininfo: Window id:' | awk -F \"\\\"\" '{ print $2 }' | tr -d '\n'" % id
 		winNameOutput, winNameError = subprocess.Popen(cmd, shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE).communicate()
 		logger.debug('winNameOutput : %s ' % winNameOutput)
 		logger.debug('winNameError  : %s'  % winNameError )
 
 		if winNameError == '' :
-			logger.info('Current Window Name : %s' % winNameOutput)
+			logger.debug('Current Window Name : %s' % winNameOutput)
 			return winNameOutput
 		else :
 			return None
@@ -61,7 +58,7 @@ class window :
 		logger.debug('winPidError  : %s ' % winPidError)
 
 		if winPidError == '' :
-			logger.info('Current Window Pid : %s' % winPidOutput)
+			logger.debug('Current Window Pid : %s' % winPidOutput)
 			return winPidOutput
 		else :
 			return None
@@ -72,11 +69,29 @@ class window :
 		logger.debug('winBinOutput : %s ' % winBinOutput)
 		logger.debug('winBinError  : %s ' % winBinError)
 
+		# If empty try to find binary via pid
+		if winBinOutput == '' :
+			pid = self.getWindowPid(id)
+			cmd = "ps aux | grep %s | grep -v grep | awk '{ print $11 }' | tr -d '\n'" % pid
+			winBinOutput, winBinError = subprocess.Popen(cmd, shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE).communicate()
+			logger.debug('winBinOutput : %s ' % winBinOutput)
+			logger.debug('winBinError  : %s ' % winBinError)
+
 		if winBinError == '' :
-			logger.info('Current Window Binary : %s' % winBinOutput)
+			logger.debug('Current Window Binary : %s' % winBinOutput)
 			return winBinOutput
 		else :
 			return None
+
+
+	def getWindowScreenshot(self, id):
+		uuid = uuid1()	
+		cmd = "xwd -id %s -out /tmp/windowlog-%s.xwd && convert /tmp/windowlog-%s.xwd /tmp/windowlog-%s.png && rm -Rf /tmp/windowlog-%s.xwd" % (id, uuid, uuid, uuid, uuid)
+		winScreenOut, winScreenError = subprocess.Popen(cmd, shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE).communicate()
+		
+		logger.debug('cmd            : %s' % cmd)
+		logger.debug('winScreenOut   : %s' % winScreenOut)
+		logger.debug('winScreenError : %s' % winScreenError)
 
 
 	def getId(self) :
@@ -108,16 +123,32 @@ class window :
 		self.setName()
 		self.setPid()
 		self.setBin()
-		
+	
 		if self.id != self.oldid :
 			logger.info('--> Window Changed <--')
+			self.getInfo()
 			self.oldid = self.id
-
+			self.getWindowScreenshot(self.id)
+		
 
 if __name__ == "__main__" :
 	
 	logger.info('Windowlog started ...')
+	
+	# Database configuration
+	#engine = create_engine('sqlite:///windowlog.db', echo=True)
+	#metadata = MetaData(bind=engine)
+	#	
+	#windowlog = Table('windowlog', metadata,
+	#		  Column('moment', Integer, primary_key=True),
+	#		  Column('windowname', String(40)))
+	#
+	#metadata.create_all()
+	#connection = engine.connect()
 
+	#newdata = windowlog.insert().values(moment = 201201012010, windowname = 'test')
+	#connection.execute(newdata)
+	
 	w = window()
 
 	while True :
@@ -135,6 +166,3 @@ if __name__ == "__main__" :
 			logger.info('Windowlog exited ...')
 			sys.exit(0)
 
-
-	#newdata = windowlog.insert().values(moment = 201201012010, windowname = 'test')
-	#connection.execute(newdata)
